c708b70ec19597443b3b2091e08467c1f16487dd
YashrajKupekar17 <ykupekar007@gmail.com>
2024-06-28 12:24:00 +0530

    categories added

--- src/components/ReduxAddToCart/ReduxAddToCart.jsx
+++ src/components/ReduxAddToCart/ReduxAddToCart.jsx
@@ -1,21 +1,24 @@
-import { useSelector, useDispatch } from "react-redux";
-function ReduxAddToCart({ product }) {
-    console.log("add to cart", product.id);
-    // useSelector
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import { addToCart } from "../../store";
+function AddToCart({ product }) {
+    // console.log("add to cart", product);
     let dispatch = useDispatch();
+    // useSelector
     function increase() {
         //dispatch({type: , payload: })
-        dispatch({ type: "ADD_TO_CART", payload: product });
+        dispatch(addToCart(product));
     }
     function decrease() {
-     //dispatch({type: , payload: })
-     dispatch({ type: "REMOVE_FROM_CART", payload: product });
+        //dispatch({type: , payload: })
+        dispatch({type: "REMOVE_FROM_CART", payload: product});
     }
 
-    let quantity = useSelector((state) => {
-        return state.items[product.id]?.quantity || 0;
-    })
-
+    let cart = useSelector((state) => {
+        return state.items;
+    });
+    // console.log("cart", cart);
+    const quantity = cart[product.id] ? cart[product.id].quantity : 0;
     if (quantity === 0) {
         return (
             <div>
@@ -32,4 +35,16 @@ function ReduxAddToCart({ product }) {
         )
     }
 }
-export default ReduxAddToCart;
+export default AddToCart;
+
+
+// array of object 
+// object of object 
+
+//[{id: 1, quantity: 10}, {id: 2, quantity: 10}, {id: 3, quantity: 10}, {id: 4, quantity: 10}]
+// cart = 
+// {id:{id: 1, quantity: 10}, id:{id: 2, quantity: 10}, id:{id: 3, quantity: 10}, id:{id: 4, quantity: 10}}
+//cart["3"]
+
+// Object.value(obj);
+// Object.keys(obj);
